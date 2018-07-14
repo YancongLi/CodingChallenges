a2b5af3e1546b795aebc9d3035461de1bf0216a2
Peter <yancong.li@alumni.ubc.ca>
2018-07-14 16:35:25 -0700

    One away strings
    One away strings

--- src/com/peter/OneAwayStrings.java
+++ src/com/peter/OneAwayStrings.java
@@ -1,7 +1,5 @@
 package com.peter;
 
-import java.util.ArrayList;
-
 /*
 Write a function that takes two strings and returns true if they are one away
 from each other.
@@ -30,70 +28,121 @@ public class OneAwayStrings {
         System.out.println(isOneAway("abc", "bcc"));  // should return false
     }
 
-    // Implement your solution below.
+    // My solution:
+//    public static Boolean isOneAway(String s1, String s2) {
+//        final int s1length = s1.length();
+//        final int s2length = s2.length();
+//
+//        if (s1length - s2length > 1 || s2length - s1length > 1) {//case1: "abc", "abcde" => eliminates invalid length difference
+//            return false;
+//        }
+//
+//        if (s1.equals(s2)) {//case2: "a", "a"
+//            return true;
+//        }
+//
+//        if (s1.contains(s2) || s2.contains(s1)) {//case3: "abcde", "abcd"
+//            return true;
+//        }
+//
+//        //case4: "abcdef", "abqdef"
+//        if (s1length == s2length) {
+//            int differenceCounter = 0;
+//            char[] c1 = s1.toCharArray();
+//            char[] c2 = s2.toCharArray();
+//            for (int i = 0; i < s1length; i++) {
+//                if (c1[i] != c2[i]) {
+//                    differenceCounter++;
+//                    if (differenceCounter > 1) {
+//                        return false;
+//                    }
+//                }
+//            }
+//            return true;
+//        }
+//
+//        //case 5:
+//        ArrayList<Character> characterArrayList1 = new ArrayList<>();
+//        ArrayList<Character> characterArrayList2 = new ArrayList<>();
+//        ArrayList<Character> tempArray;
+//        for (char c : s1.toCharArray()) {
+//            characterArrayList1.add(c);
+//        }
+//        for (char c : s2.toCharArray()) {
+//            characterArrayList2.add(c);
+//        }
+//
+//        if (s1length > s2length) {//"abcde", "abde"
+//            for (int i = 1; i < s1length - 1; i++) {
+//                tempArray = (ArrayList<Character>) characterArrayList1.clone();
+//                tempArray.remove(i);
+//                String tempStr = tempArray.toString();
+//                if (tempStr.equals(characterArrayList2.toString())) {
+//                    return true;
+//                }
+//            }
+//        } else {//"abde", "abcde"
+//            for (int i = 1; i < s2length - 1; i++) {
+//                tempArray = (ArrayList<Character>) characterArrayList2.clone();
+//                tempArray.remove(i);
+//                String tempStr = tempArray.toString();
+//                if (tempStr.equals(characterArrayList1.toString())) {
+//                    return true;
+//                }
+//            }
+//        }
+//
+//        return false;
+//    }
+
+    //A better solution:
     public static Boolean isOneAway(String s1, String s2) {
         final int s1length = s1.length();
         final int s2length = s2.length();
 
-        if (s1length - s2length > 1 || s2length - s1length > 1) {//case1: "abc", "abcde" => eliminates invalid length difference
+        if (s1length - s2length > 1 || s2length - s1length > 1) {//eliminates invalid length difference
             return false;
         }
 
-        if (s1.equals(s2)) {//case2: "a", "a"
-            return true;
+        if (s1length == s2length) {//case: same length
+            return sameLengthHelper(s1, s2, s1length);
         }
 
-        if (s1.contains(s2) || s2.contains(s1)) {//case3: "abcde", "abcd"
-            return true;
+        if (s1length > s2length) {//case: valid length
+            return diffLengthHelper(s1, s2, s2length);
+        } else {
+            return diffLengthHelper(s2, s1, s1length);
         }
 
-        //case4: "abcdef", "abqdef"
-        if (s1length == s2length) {
-            int differenceCounter = 0;
-            char[] c1 = s1.toCharArray();
-            char[] c2 = s2.toCharArray();
-            for (int i = 0; i < s1length; i++) {
-                if (c1[i] != c2[i]) {
-                    differenceCounter++;
-                    if (differenceCounter > 1) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-        //case 5:
-        ArrayList<Character> characterArrayList1 = new ArrayList<>();
-        ArrayList<Character> characterArrayList2 = new ArrayList<>();
-        ArrayList<Character> tempArray;
-        for (char c : s1.toCharArray()) {
-            characterArrayList1.add(c);
-        }
-        for (char c : s2.toCharArray()) {
-            characterArrayList2.add(c);
-        }
-
-        if (s1length > s2length) {//"abcde", "abde"
-            for (int i = 1; i < s1length - 1; i++) {
-                tempArray = (ArrayList<Character>) characterArrayList1.clone();
-                tempArray.remove(i);
-                String tempStr = tempArray.toString();
-                if (tempStr.equals(characterArrayList2.toString())) {
-                    return true;
-                }
-            }
-        } else {//"abde", "abcde"
-            for (int i = 1; i < s2length - 1; i++) {
-                tempArray = (ArrayList<Character>) characterArrayList2.clone();
-                tempArray.remove(i);
-                String tempStr = tempArray.toString();
-                if (tempStr.equals(characterArrayList1.toString())) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
+
+    private static boolean sameLengthHelper(String s1, String s2, int length) {
+        int diffCount = 0;
+        for (int i = 0; i < length; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diffCount++;
+                if (diffCount > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static Boolean diffLengthHelper(String s1, String s2, int smallerLength) {//s1Length > s2Length
+        int pointer = 0;
+        int diffCount = 0;
+        while (pointer < smallerLength) {
+            if (s1.charAt(pointer + diffCount) == s2.charAt(pointer)) {
+                pointer++;
+            }else {
+                diffCount++;
+                if (diffCount>1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
