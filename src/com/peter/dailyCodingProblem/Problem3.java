ef2c6748d719c332de111ee9b236a5d2acfa36db
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-07-16 22:53:34 -0700

    daily p3
    daily p3

--- src/com/peter/dailyCodingProblem/Problem3.java
+++ src/com/peter/dailyCodingProblem/Problem3.java
@@ -19,4 +19,36 @@ assert deserialize(serialize(node)).left.left.val == 'left.left'
 */
 
 public class Problem3 {
+    public static void main(String[] args) {
+        Node testNode
+                = new Node("root", new Node("left", new Node("left.left", null, null), null), new Node("right", null , null));
+
+        String nodeString =  Solution.serialize(testNode);
+        System.out.println(nodeString);
+        String result = Solution.deserialize(nodeString).left.left.val;
+        System.out.println(result);// should return "left.left"
+    }
+
+    public static class Solution {
+        // Encodes a tree to a single string.
+        static String serialize(Node root) {
+            return "";
+        }
+
+        // Decodes your encoded data to tree.
+        static Node deserialize(String data) {
+            return new Node("", null, null);
+        }
+    }
+
+    public static class Node {
+        String val;
+        Node left, right;
+
+        Node(String value, Node left, Node right) {
+            this.val = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
