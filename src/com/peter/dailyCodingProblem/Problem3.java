da7bcf96085795ae6dce0a99d7461d655d32f695
Peter <yancong.li@alumni.ubc.ca>
2018-07-17 02:23:03 -0700

    daily p3
    daily p3

--- src/com/peter/dailyCodingProblem/Problem3.java
+++ src/com/peter/dailyCodingProblem/Problem3.java
@@ -21,23 +21,37 @@ assert deserialize(serialize(node)).left.left.val == 'left.left'
 public class Problem3 {
     public static void main(String[] args) {
         Node testNode
-                = new Node("root", new Node("left", new Node("left.left", null, null), null), new Node("right", null , null));
+                = new Node("root", new Node("left", new Node("left.left", null, null), null), new Node("right", null, null));
 
-        String nodeString =  Solution.serialize(testNode);
-        System.out.println(nodeString);
+        String nodeString = Solution.serialize(testNode);
+        //System.out.println(nodeString);
         String result = Solution.deserialize(nodeString).left.left.val;
         System.out.println(result);// should return "left.left"
     }
 
     public static class Solution {
+        static final String separator = ",";
+
         // Encodes a tree to a single string.
         static String serialize(Node root) {
-            return "";
+            if (root == null) {
+                return "null";
+            }
+            return root.val + separator + serialize(root.left) + separator + serialize(root.right);
         }
 
         // Decodes your encoded data to tree.
         static Node deserialize(String data) {
-            return new Node("", null, null);
+            String[] temp = data.split(separator);
+            return deserializeStringArray(temp, new int[]{0});
+        }
+
+        private static Node deserializeStringArray(String[] temp, int[] index) {
+            if (index[0] > temp.length || temp[index[0]].equals("null")) {
+                index[0]++;
+                return null;
+            }
+            return new Node(temp[index[0]++], deserializeStringArray(temp, index), deserializeStringArray(temp, index));
         }
     }
 
