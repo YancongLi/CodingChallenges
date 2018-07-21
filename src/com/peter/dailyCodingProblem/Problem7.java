4c1e19db6d512fa3b795c0ddd8ec5fcc75d51a98
Peter <yancong.li@alumni.ubc.ca>
2018-07-21 16:58:26 -0700

    daily p7
    daily p7

--- src/com/peter/dailyCodingProblem/Problem7.java
+++ src/com/peter/dailyCodingProblem/Problem7.java
@@ -10,6 +10,42 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 
 public class Problem7 {
     public static void main(String[] args) {
+        String str1 = "111";
+        System.out.println(numOfWays(str1)); // should return 3 ("aaa", "ka", and "ak").
 
+        String str2 = "";
+        System.out.println(numOfWays(str2)); // should return 1
+
+        String str3 = "011";
+        System.out.println(numOfWays(str3)); // should return 0
+
+        String str4 = "1234";
+        System.out.println(numOfWays(str4)); // should return 3 ("abcd", "lcd", "awd")
+
+        String str5 = "0";
+        System.out.println(numOfWays(str5)); // should return 0
+
+        String str6 = "1203";
+        System.out.println(numOfWays(str6)); // should return 1 ("atc")
+
+        String str7 = "226";
+        System.out.println(numOfWays(str7)); // should return 3 ("bz", "vf", "bbf")
+    }
+
+    private static int numOfWays(String str) {
+        if (str.length() >=1 ){
+            if (str.charAt(0) == '0') {
+                return 0;
+            }
+        }
+        if (str.length() <= 1) {
+            return 1;
+        }
+        int sum = 0;
+        if (Integer.parseInt(str.substring(0, 2)) <= 26) {
+            sum = numOfWays(str.substring(2));
+        }
+        sum += numOfWays(str.substring(1));
+        return sum;
     }
 }
