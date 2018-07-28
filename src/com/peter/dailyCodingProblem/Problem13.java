672f21c3764ccaac4648357a056f7a187d50a50f
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-07-28 13:47:59 -0700

    daily p13
    daily p13

---
+++ src/com/peter/dailyCodingProblem/Problem13.java
@@ -0,0 +1,13 @@
+package com.peter.dailyCodingProblem;
+
+/*
+Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+*/
+
+public class Problem13 {
+    public static void main(String[] args) {
+
+    }
+}
