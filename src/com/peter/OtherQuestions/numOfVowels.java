b8a49f0add3bf48f2190dfcaea81248275bfb667
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-07-27 22:36:01 -0700

    refactor
    refactor

--- src/com/peter/OtherQuestions/numOfVowels.java
+++ src/com/peter/OtherQuestions/numOfVowels.java
@@ -8,43 +8,37 @@ Create a method that takes a string input and prints out and number of vowels an
 (Vowels: a,e,i,o,u,y)
 */
 
-import javafx.util.Pair;
-import java.util.ArrayList;
-
 public class numOfVowels {
     public static void main(String[] args) {
         String str1 = "HellO";
         String str2 = "there is a quiet Mouse";
         String str3 = "I am happy";
 
-        System.out.println(numOfVowelsSolution(str1).getKey() + " vowel(s), " + numOfVowelsSolution(str1).getValue() + " consonant(s).");//should return 2 vowels, 3 consonants
-        System.out.println(numOfVowelsSolution(str2).getKey() + " vowel(s), " + numOfVowelsSolution(str2).getValue() + " consonant(s).");//should return 10 vowels, 8 consonants
-        System.out.println(numOfVowelsSolution(str3).getKey() + " vowel(s), " + numOfVowelsSolution(str3).getValue() + " consonant(s).");//should return 4 vowels, 4 consonants
+        numOfVowelsSolution(str1);//should return 2 vowels, 3 consonants
+        numOfVowelsSolution(str2);//should return 10 vowels, 8 consonants
+        numOfVowelsSolution(str3);//should return 4 vowels, 4 consonants
     }
 
-    private static Pair<Integer, Integer> numOfVowelsSolution(String str) {
-        ArrayList<Character> listOfVowels = new ArrayList<Character>() {{
-            add('a');
-            add('e');
-            add('i');
-            add('o');
-            add('u');
-            add('y');
-        }};
+    private static void numOfVowelsSolution(String str) {
+        String vowels = "aeiouf";
         int numOfVowels = 0;
+        int numOfConsonants = 0;
         String[] normalized = str.split(" ");
         StringBuilder stringBuilder = new StringBuilder();
-        for (String string: normalized) {
+        for (String string : normalized) {
             stringBuilder.append(string);
         }
         String normalizedStr = stringBuilder.toString();
         int length = normalizedStr.length();
 
         for (Character c : normalizedStr.toLowerCase().toCharArray()) {
-            if (listOfVowels.contains(c)) {
+            if (vowels.indexOf(c) != -1) {
                 numOfVowels++;
             }
         }
-        return new Pair<>(numOfVowels, length - numOfVowels);
+        numOfConsonants = length - numOfVowels;
+        String placeholder1 = numOfVowels > 1 ? "Number of Vowels: " : "Number of Vowel: ";
+        String placeholder2 = numOfConsonants > 1 ? " | Number of consonants: " : " | Number of consonant: ";
+        System.out.println(placeholder1 + numOfVowels + placeholder2 + numOfConsonants);
     }
 }
