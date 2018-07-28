7742c84ffcf9c2a8027569b0e37fd7717583a57b
Yancong(Peter) Li <yancong.li@alumni.ubc.ca>
2018-07-28 13:48:14 -0700

    daily p14
    daily p14

---
+++ src/com/peter/dailyCodingProblem/Problem14.java
@@ -0,0 +1,13 @@
+package com.peter.dailyCodingProblem;
+
+/*
+The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+
+Hint: The basic equation of a circle is x2 + y2 = r2.
+*/
+
+public class Problem14 {
+    public static void main(String[] args) {
+
+    }
+}
