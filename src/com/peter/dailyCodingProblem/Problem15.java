ba3ce17cfb88513a33977c4c4fe2b234b4bc63d8
Peter <yancong.li@alumni.ubc.ca>
2018-07-29 15:02:28 -0700

    daily p15
    daily p15

---
+++ src/com/peter/dailyCodingProblem/Problem15.java
@@ -0,0 +1,11 @@
+package com.peter.dailyCodingProblem;
+
+/*
+Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
+*/
+
+public class Problem15 {
+    public static void main(String[] args) {
+        
+    }
+}
