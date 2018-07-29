0e513bba5eb6edb160dd14579ed396be7d369d7b
Peter <yancong.li@alumni.ubc.ca>
2018-07-29 16:45:14 -0700

    daily p13
    daily p13

--- src/com/peter/dailyCodingProblem/Problem13.java
+++ src/com/peter/dailyCodingProblem/Problem13.java
@@ -8,6 +8,33 @@ For example, given s = "abcba" and k = 2, the longest substring with k distinct 
 
 public class Problem13 {
     public static void main(String[] args) {
+        String string1 = "abcba";
+        System.out.println(longestSubStrWithKDistinctChar(string1, 2));//should return "bcb"
+    }
 
+    private static String longestSubStrWithKDistinctChar(String str, int k) {
+        String result = "-1";
+        int length = str.length();
+        if (k > length) {
+            return result;
+        }
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < length; i++) {
+            int j = i + 1;
+            int diffCount = 0;
+            String temp = str.substring(i,j);
+            while (j < length) {
+                if (temp.indexOf(chars[j]) == -1) {
+                    diffCount ++;
+                    if (diffCount == k) {
+                        if (str.substring(i, j).length() > result.length()) {
+                            result = str.substring(i, j);
+                        }
+                    }
+                }
+                j++;
+            }
+        }
+        return result;
     }
 }
