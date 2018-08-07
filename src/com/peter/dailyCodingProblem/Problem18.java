8679c6e3009b2aa8d60dc817e77f3b3aa88b5c5d
Peter <yancong.li@alumni.ubc.ca>
2018-08-06 22:11:21 -0700

    O(n * k) solution

--- src/com/peter/dailyCodingProblem/Problem18.java
+++ src/com/peter/dailyCodingProblem/Problem18.java
@@ -1,4 +1,8 @@
 package com.peter.dailyCodingProblem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
 Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
 
@@ -12,6 +16,32 @@ Do this in O(n) time and O(k) space. You can modify the input array in-place and
 */
 public class Problem18 {
     public static void main(String[] args) {
+        int[] a1 = {10, 5, 2, 7, 8, 7};
+        System.out.println(Arrays.toString(maxValuesOfSubarray(a1, 3)));//should return [10, 7, 8, 8]
+    }
 
+    private static int[] maxValuesOfSubarray(int[] a, int k) {//this is O(n * k)
+        ArrayList<Integer> resultArray = new ArrayList<>();
+        ArrayList<Integer> inputArray = new ArrayList<>();
+        for (int i : a) {
+            inputArray.add(i);
+        }
+        ArrayList<Integer> subArray = new ArrayList<>(inputArray.subList(0, inputArray.size()));
+        for (int i = 0; i < inputArray.size() - k + 1; i++) {
+            int max = subArray.get(0);
+            for (int j = 0; j < k; j++) {
+                if (subArray.get(j) > max) {
+                    max = subArray.get(j);
+                }
+            }
+            resultArray.add(max);
+            subArray = new ArrayList<>(inputArray.subList(i + 1, inputArray.size()));
+            //System.out.println("index i: " + i + "|" + subArray);
+        }
+        int[] result = new int[resultArray.size()];
+        for (int i = 0; i < resultArray.size(); i++) {
+            result[i] = resultArray.get(i);
+        }
+        return result;
     }
 }
