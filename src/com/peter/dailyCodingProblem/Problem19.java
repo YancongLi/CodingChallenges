32767ad27b70899a1d4fad067c1316c178063b9d
Peter <yancong.li@alumni.ubc.ca>
2018-08-07 23:21:14 -0700

    commented for future reference

--- src/com/peter/dailyCodingProblem/Problem19.java
+++ src/com/peter/dailyCodingProblem/Problem19.java
@@ -4,6 +4,10 @@ A builder is looking to build a row of N houses that can be of K different color
 
 Given an N by K matrix where the nth row and kth column represents the cost to build the nth house with kth color, return the minimum cost which achieves this goal.
 */
+
+//see:
+//https://www.programcreek.com/2014/05/leetcode-paint-house-ii-java/
+
 public class Problem19 {
     public static void main(String[] args) {
 
