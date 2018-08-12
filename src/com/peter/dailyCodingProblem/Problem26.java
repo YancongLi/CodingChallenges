d363b0304686aa61f2ff6227dd23b1124a4055bd
Peter <yancong.li@alumni.ubc.ca>
2018-08-12 14:18:52 -0700

    added p26-p29

---
+++ src/com/peter/dailyCodingProblem/Problem26.java
@@ -0,0 +1,13 @@
+package com.peter.dailyCodingProblem;
+/*
+Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length of the list.
+
+The list is very long, so making more than one pass is prohibitively expensive.
+
+Do this in constant space and in one pass.
+*/
+public class Problem26 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/peter/dailyCodingProblem/Problem27.java
@@ -0,0 +1,13 @@
+package com.peter.dailyCodingProblem;
+/*
+Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
+
+For example, given the string "([])[]({})", you should return true.
+
+Given the string "([)]" or "((()", you should return false.
+*/
+public class Problem27 {
+    public static void main(String[] args) {
+
+    }
+}
