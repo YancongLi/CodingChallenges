3306e3daa0504c5ea7955a3190e9f9c7c3e488b4
Peter <yancong.li@alumni.ubc.ca>
2018-08-12 17:58:23 -0700

    Used stack

--- src/com/peter/dailyCodingProblem/Problem27.java
+++ src/com/peter/dailyCodingProblem/Problem27.java
@@ -1,4 +1,7 @@
 package com.peter.dailyCodingProblem;
+
+import java.util.*;
+
 /*
 Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
 
@@ -8,6 +11,37 @@ Given the string "([)]" or "((()", you should return false.
 */
 public class Problem27 {
     public static void main(String[] args) {
+        System.out.println(areBracketsBalanced("([])[]({})"));//should return true
+        System.out.println(areBracketsBalanced("()[]{}"));//should return true
+        System.out.println(areBracketsBalanced("{[]}"));//should return true
+        System.out.println(areBracketsBalanced("([)]"));//should return false
+        System.out.println(areBracketsBalanced("((()"));//should return false
+    }
 
+    private static boolean areBracketsBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        Set<Character> set = new HashSet<>();
+        set.add('(');
+        set.add('[');
+        set.add('{');
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (set.contains(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (map.get(c) != stack.peek()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
     }
 }
