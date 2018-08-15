6e920244a3bd54e6e12ab0b3930fcb055c0ce851
Peter <yancong.li@alumni.ubc.ca>
2018-08-14 22:45:11 -0700

    added p30 and p31

---
+++ src/com/peter/dailyCodingProblem/Problem31.java
@@ -0,0 +1,11 @@
+package com.peter.dailyCodingProblem;
+/*
+The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions required to change one string to the other. For example, the edit distance between “kitten” and “sitting” is three: substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+
+Given two strings, compute the edit distance between them.
+*/
+public class Problem31 {
+    public static void main(String[] args) {
+
+    }
+}
