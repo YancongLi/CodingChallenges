0e79c3078426fd2ad07ce6413961bb9e0f94c5d4
Peter <yancong.li@alumni.ubc.ca>
2018-08-20 22:02:53 -0700

    adde p34-37

---
+++ src/com/peter/dailyCodingProblem/Problem36.java
@@ -0,0 +1,9 @@
+package com.peter.dailyCodingProblem;
+/*
+Given the root to a binary search tree, find the second largest node in the tree.
+*/
+public class Problem36 {
+    public static void main(String[] args) {
+
+    }
+}
