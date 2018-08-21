caed00ec552e8cdae4268198bac7f1930fc7adff
Peter <yancong.li@alumni.ubc.ca>
2018-08-20 21:57:51 -0700

    Done

--- src/com/peter/dailyCodingProblem/Problem33.java
+++ src/com/peter/dailyCodingProblem/Problem33.java
@@ -1,4 +1,8 @@
 package com.peter.dailyCodingProblem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 /*
 Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element.
 
@@ -16,6 +20,27 @@ For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should pri
 */
 public class Problem33 {
     public static void main(String[] args) {
-        
+        int[] sequence1 = {2, 1, 5, 7, 2, 0, 5};
+        printRunningMedians(sequence1);
+    }
+
+    private static void printRunningMedians(int[] input) {
+        int lengthOfInput = input.length;
+        ArrayList<Integer> sortedInput = new ArrayList<>();
+        for (int i = 0; i < lengthOfInput; i++) {
+            sortedInput.add(input[i]);
+            Collections.sort(sortedInput);
+            double median = getMedianForSortedList(sortedInput);
+            System.out.println(median);
+        }
+    }
+
+    private static double getMedianForSortedList(ArrayList<Integer> sortedInput) {
+        int length = sortedInput.size();
+        if (length % 2 == 0) {
+            return (double) (sortedInput.get(length / 2) + sortedInput.get(length / 2 - 1)) / 2;
+        } else {
+            return sortedInput.get(length / 2);
+        }
     }
 }
