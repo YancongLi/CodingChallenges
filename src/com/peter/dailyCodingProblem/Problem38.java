e018a8858fc173bb4950d6a340750aef50046b09
Peter <yancong.li@alumni.ubc.ca>
2018-08-23 23:15:13 -0700

    added p38-p41

---
+++ src/com/peter/dailyCodingProblem/Problem38.java
@@ -0,0 +1,9 @@
+package com.peter.dailyCodingProblem;
+/*
+You have an N by N board. Write a function that, given N, returns the number of possible arrangements of the board where N queens can be placed on the board without threatening each other, i.e. no two queens share the same row, column, or diagonal.
+*/
+public class Problem38 {
+    public static void main(String[] args) {
+
+    }
+}
