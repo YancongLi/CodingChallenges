1efc7b68211025c04d1798740a2c272d29dc3f8c
Peter <yancong.li@alumni.ubc.ca>
2018-08-29 00:58:36 -0700

    added p42 - p45

---
+++ src/com/peter/dailyCodingProblem/Problem45.java
@@ -0,0 +1,9 @@
+package com.peter.dailyCodingProblem;
+/*
+Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability, implement a function rand7() that returns an integer from 1 to 7 (inclusive).
+*/
+public class Problem45 {
+    public static void main(String[] args) {
+
+    }
+}
