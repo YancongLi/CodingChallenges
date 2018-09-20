0e9bec08ac3f927903904f6ab5bd56a4bc7a4fd6
Peter <yancong.li@alumni.ubc.ca>
2018-09-19 23:38:53 -0700

    studied p41

--- src/com/peter/dailyCodingProblem/Problem41.java
+++ src/com/peter/dailyCodingProblem/Problem41.java
@@ -1,4 +1,8 @@
 package com.peter.dailyCodingProblem;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the person's itinerary. If no such itinerary exists, return null. If there are multiple possible itineraries, return the lexicographically smallest one. All flights must be used in the itinerary.
 
@@ -10,6 +14,60 @@ Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and s
 */
 public class Problem41 {
     public static void main(String[] args) {
+        HashMap<String, String> dataSet = new HashMap<>();
+//        dataSet.put("SFO", "HKO");
+//        dataSet.put("YYZ", "SFO");
+//        dataSet.put("YUL", "YYZ");
+//        dataSet.put("HKO", "ORD");
+//        dataSet.put("Chennai", "Banglore");
+//        dataSet.put("Bombay", "Delhi");
+//        dataSet.put("Goa", "Chennai");
+//        dataSet.put("Delhi", "Goa");
 
+        //If you try the below case, it will loop infinitely, however, it works with above examples
+        dataSet.put("A", "B");
+        dataSet.put("A", "C");
+        dataSet.put("B", "C");
+        dataSet.put("C", "A");
+
+        printResult(dataSet);
+    }
+
+    private static void printResult(HashMap<String, String> dataSet) {
+        // To store reverse of given map
+        HashMap<String, String> reverseMap = new HashMap<>();
+
+        // To fill reverse map, iterate through the given map
+        for (Map.Entry<String,String> entry: dataSet.entrySet())
+            reverseMap.put(entry.getValue(), entry.getKey());
+
+        // Find the starting point of itinerary
+        String start = null;
+        for (Map.Entry<String,String> entry: dataSet.entrySet())
+        {
+            if (!reverseMap.containsKey(entry.getKey()))
+            {
+                start = entry.getKey();
+                break;
+            }
+        }
+
+        // If we could not find a starting point, then something wrong
+        // with input
+        if (start == null)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        // Once we have starting point, we simple need to go next, next
+        // of next using given hash map
+        String to = dataSet.get(start);
+        while (to != null)
+        {
+            System.out.print(start +  "->" + to + ", ");
+            start = to;
+            to = dataSet.get(to);
+        }
     }
 }
